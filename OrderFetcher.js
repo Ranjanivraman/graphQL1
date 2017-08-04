7c9a09b4ddbd8f9deb2d09af0b9bbdaaafc88746
Doug Dickinson <doug.dickinson@missguided.com>
2017-08-04 17:56:09 +0100

    Put limit back up (wifi was the problem); added note about pagination

--- OrderFetcher.js
+++ OrderFetcher.js
@@ -3,8 +3,8 @@ import { mgFetchMany } from './MGFetcher';
 
 export const OrderFetcher = {
   getOrders(customerId) {
-    // N.B. I've had to put a limit on the number of orders here because sometimes fetching many orders causes many many product fetches and somebody gets confused.  I don't know if it is this graphql server or the magento server.
-    const url = `https://api-test2.mgnonprod.co.uk/api/rest/orders?limit=2&order=created_at&dir=dsc&filter[1][attribute]=customer_id&filter[1][in]=${customerId}`;
+    // TODO: should do pagination, but not needed for prototype and besides I don't understand magento's wacky page parameter!  
+    const url = `https://api-test2.mgnonprod.co.uk/api/rest/orders?limit=99&order=created_at&dir=dsc&filter[1][attribute]=customer_id&filter[1][in]=${customerId}`;
 
     return mgFetchMany(url);
   },
