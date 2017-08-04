9ea9fbd9e8ad326c1a4a6326ce7ecf8478ed1dc8
Doug Dickinson <doug.dickinson@missguided.com>
2017-08-04 14:00:37 +0100

    Limit orders to 2; sort by date descending
    Orders limited to 2 because for some reason sometimes either the graphql server or magento times out when there are too many (i'm not sure if it is too many orders or too many products).  I'm not going to investigate because 1) the orders should be fetched using pagination and 2) the graphql 'server' is just a prototype

--- OrderFetcher.js
+++ OrderFetcher.js
@@ -3,7 +3,8 @@ import { mgFetchMany } from './MGFetcher';
 
 export const OrderFetcher = {
   getOrders(customerId) {
-    const url = `https://api-test2.mgnonprod.co.uk/api/rest/orders?filter[1][attribute]=customer_id&filter[1][in]=${customerId}`;
+    // N.B. I've had to put a limit on the number of orders here because sometimes fetching many orders causes many many product fetches and somebody gets confused.  I don't know if it is this graphql server or the magento server.
+    const url = `https://api-test2.mgnonprod.co.uk/api/rest/orders?limit=2&order=created_at&dir=dsc&filter[1][attribute]=customer_id&filter[1][in]=${customerId}`;
 
     return mgFetchMany(url);
   },
